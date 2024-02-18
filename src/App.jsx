b8f795d12d3032fc8648de538f0cd3686ef89309
Fanpeng Li <serforfp@gmail.com>
2024-02-18 13:04:36 +0000

    Setup create vite react project, created main components

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar";
+import Context from "./components/Context";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Content />
+      <Context />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <nav>
+      <ul className="header">
+        <li className="nav-item">Nav 1</li>
+        <li className="nav-item">Nav 2</li>
+        <li className="nav-item">Nav 3</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
