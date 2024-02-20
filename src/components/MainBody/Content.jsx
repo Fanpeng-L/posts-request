7aee14df47a58fd86cbad9476627072136396dfb
Fanpeng Li <serforfp@gmail.com>
2024-02-20 23:14:40 +0000

    Added show all posts button

--- src/components/MainBody/Content.jsx
+++ src/components/MainBody/Content.jsx
@@ -6,6 +6,7 @@ function Content() {
 
   const [data, setData] = useState([]);
   const [selectedPost, setSelectedPost] = useState(null);
+  const [showAllPosts, setShowAllPosts] = useState(false);
 
   const fetchData = async () => {
     try {
@@ -24,24 +25,33 @@ function Content() {
     const postId = parseInt(event.target.value);
     // find the post id with the selected option id
     const post = data.find((post) => post.id === postId);
+    setShowAllPosts(false);
     console.log(post);
     setSelectedPost(post);
+  };
+
+  const handleShowAllButton = () => {
+    setShowAllPosts(true);
   };
 
   return (
     <div>
       <h1>Select the Post based on UserID:</h1>
-      <select onChange={handlePostSelect}>
-        {[...Array(100)].map((_, index) => (
-          <option key={index + 1} value={index + 1}>
-            Post {index + 1}
-          </option>
-        ))}
-      </select>
+      <div className="select-box">
+        <select onChange={handlePostSelect}>
+          {[...Array(100)].map((_, index) => (
+            <option key={index + 1} value={index + 1}>
+              Post {index + 1}
+            </option>
+          ))}
+        </select>
+        <button className="showAll-btn" onClick={handleShowAllButton}>
+          Show All Posts
+        </button>
+      </div>
 
-      {selectedPost ? (
+      {selectedPost && !showAllPosts ? (
         <div>
-          <h2>Selected Post</h2>
           <h3>{selectedPost.title}</h3>
           <p>{selectedPost.body}</p>
         </div>
